8f95454e537c8d25c07e6f562a707884438d3bea
biaCJ2008 <beatriz_jose@estudante.sesisenai.org.br>
2025-04-15 17:03:39 -0300

    PHP

---
+++ PHP/Beatriz_Jose/Array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $dias = array('domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado');
+    echo $dias[1]."<br>";
+    print_r($dias);
+    echo"<br>";
+    var_dump($dias);
+    echo"<br>";
+    echo"<br>";
+    echo "Beatriz Coimbra José";
+    ?>
+</body>
+</html>
---
+++ PHP/Beatriz_Jose/testeIf.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $umidade =91;
+    //Testa se $umidade maior que 90. Retorna um booleano 
+    $vaiChover = ($umidade<90);
+    if ($vaiChover){
+        echo "vai chover com toda certeza absoluta na terra. <br>";
+    }
+
+    echo"<em>Beatriz Coimbra José</em>"
+    ?>
+</body>
+</html>
